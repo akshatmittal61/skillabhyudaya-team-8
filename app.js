ccabaa19fc0d418557c3f16a0cbc9ed81df02d41
akshatmittal61 <akshatmittal2506@gmail.com>
2022-03-01 01:19:02 +0530

    added scroll animations

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const header = document.querySelector("header");
+const backImg = document.querySelector(".back-img");
 const shadowValues = [2, 4, 6, 8, 12, 16, 24];
 const colors = [
 	"red",
@@ -16,3 +18,43 @@ const colors = [
 	"grey",
 	"blue-grey",
 ];
+AOS.init();
+let backImgBgColor = "rgba(240,240,240,0)";
+let headerIn = "-15vh";
+let headerBgColor = "transparent";
+let headerHeight = "15vh";
+let headerBoxShadow = "none";
+const vh = window.innerHeight / 100;
+const t = 50 * vh;
+window.addEventListener("scroll", () => {
+	if (window.scrollY < 100 * vh) {
+		backImgBgColor = `rgba(240,240,240,${window.scrollY / (100 * vh)})`;
+		headerIn = `${15 * (window.scrollY / (100 * vh) - 1)}vh`;
+		headerBgColor = "transparent";
+	} else {
+		backImgBgColor = `var(--bgcolor)`;
+		headerIn = "0";
+		if (window.scrollY < 150 * vh) {
+			bgcolor = `rgba(159, 86, 46, ${window.scrollY / t})`;
+			headerBoxShadow =
+				window.scrollY < 0.25 * t
+					? "none"
+					: `var(--shadow-elevation-${
+							window.scrollY > 0.75 * t ? 4 : 2
+					  }dp)`;
+			headerHeight = `${25 - window.scrollY / (10 * vh)}vh`;
+		} else {
+			bgcolor = "rgb(159, 86, 46)";
+			headerBoxShadow = `var(--shadow-elevation-${
+				window.scrollY > 1.25 * t ? 8 : 4
+			}dp)`;
+			headerHeight = "10vh";
+		}
+	}
+	console.log(headerHeight);
+	backImg.style.backgroundColor = backImgBgColor;
+	header.style.top = headerIn;
+	header.style.backgroundColor = headerBgColor;
+	header.style.boxShadow = headerBoxShadow;
+	header.style.height = headerHeight;
+});
